54ae18c284dfa50fd5fdab9aee2aa46e7509360f
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-07-14 17:41:05 +0300

    Add CustomUserManager

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^auth/', include('djoser.urls')),
-    url(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/', include('users.urls')),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        username,
+        password,
+        first_name,
+        last_name,
+    ):
+        user = self.model(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.is_superuser = False
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        username,
+        password,
+        first_name,
+        last_name,
+    ):
+        user = self.model(
+            email=email,
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField
