552e207e1f3963434ea99c7a1111e346c55b980c
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-07-17 21:04:04 +0300

    Add IngredientsModel

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_('Название'))
+    quantity = models.IntegerField(max_length=10, verbose_name=_('Количество'))
+    measure = models.CharField(max_length=10, verbose_name=_('Мера измерения'))
+
+    class Meta:
+        verbose_name = _('Ингредиент')
+        verbose_name_plural = _('Ингредиенты')
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
