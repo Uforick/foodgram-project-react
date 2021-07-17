6930dbe64522cb9bb8d91dd2732a22e0b303aab2
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-07-17 20:11:38 +0300

    Add Follow in users app

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.validators import UniqueTogetherValidator
+from .models import CustomUser, Follow
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,3 +8,28 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=CustomUser.objects.all()
+    )
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+        validators = [UniqueTogetherValidator(
+            queryset=Follow.objects.all(),
+            fields=['user', 'following']
+        )]
+
+    def validate(self, data):
+        if self.context['request'].user != data.get('following'):
+            return data
+        raise serializers.ValidationError('Подписка на самого себя')
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,12 @@
+from rest_framework import filters, viewsets
 from rest_framework.generics import (
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView
 )
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsOwnerOrReadOnly
-from .serializers import UserSerializer
-from .models import CustomUser
+from .serializers import UserSerializer, FollowSerializer
+from .models import CustomUser, Follow
 
 
 class UserProfileListView(ListCreateAPIView):
@@ -22,3 +23,19 @@ class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsOwnerOrReadOnly, IsAuthenticated]
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+    http_method_names = ('get', 'post')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = user.following.all()
+        return queryset
