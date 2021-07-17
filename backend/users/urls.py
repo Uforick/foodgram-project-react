4c417129ac9ee8461c118334133bcdec6f724ddb
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-07-17 20:36:43 +0300

    Add Follow url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import UserProfileDetailView, UserProfileListView
+from django.urls.conf import include
+from rest_framework.routers import DefaultRouter
+from .views import UserProfileDetailView, UserProfileListView, FollowViewSet
+
+router = DefaultRouter()
+
+
+router.register(
+    r'follow',
+    FollowViewSet,
+    basename='follow'
+)
 
 
 urlpatterns = [
     path('', UserProfileListView.as_view(), name='all-profiles'),
     path('<int:pk>/', UserProfileDetailView.as_view(), name='profile'),
+    path('', include(router.urls))
 
 ]
