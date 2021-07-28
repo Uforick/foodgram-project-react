e5b27ee5968f0e78fe0695dfe240fc1eeca5edca
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-07-28 12:37:33 +0300

    Add redoc in rec_static

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/', include('recipes.urls')),
     path('api/', include('djoser.urls')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
