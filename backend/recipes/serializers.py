1bc89548e18bbc48c106dbd95a016b9364bc4a40
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-08-04 10:12:42 +0300

    First_ReWork_Done_2

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
-
 from users.serializers import UserSerializer
 
 from .fields import Base64ImageField
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework import serializers
-
 from recipes.models import Follow, Recipe
+from rest_framework import serializers
 
 User = get_user_model()
 
